6e880d3e29d8edd6434875800795aad6da502893
asalex04 <arc04@mail.ru>
2018-02-08 07:14:53 +0400

    fixed some remarks

--- bars.py
+++ bars.py
@@ -4,60 +4,62 @@ from sys import argv
 
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding='utf-8') as json_file:
+    with open(filepath) as json_file:
         bars = json.load(json_file)
     return bars
 
 
 def get_biggest_bar(size):
     max_bar = max(bars, key=lambda x: x['Cells']['SeatsCount'])
-    ind_max = max_bar['Number']
-    return ind_max
+    return max_bar['Number']-1
 
 
 def get_smallest_bar(size):
     min_bar = min(bars, key=lambda x: x['Cells']['SeatsCount'])
-    ind_min = min_bar['Number']
-    return ind_min
+    return min_bar['Number']-1
 
 
 def get_closest_bar(distance):
-    my_longitude = float(input('введите значение долготы:'))
-    my_latitude = float(input('введите значение широты:'))
     min_dist = 99999
     index = 0
     for ind in range(len(bars)):
         dist = ((bars[ind]['Cells']['geoData']['coordinates'][0] -
-                my_longitude)**2 +
+                 my_longitude) ** 2 +
                 (bars[ind]['Cells']['geoData']['coordinates'][1] -
-                my_latitude)**2)**0.5
+                 my_latitude) ** 2) ** 0.5
         if dist < min_dist:
             min_dist = dist
             index = ind
-    return index
+        return index
 
 
 if __name__ == '__main__':
-    filepath = argv[1]
-    if os.path.isfile(filepath):
+
+    try:
+        filepath = argv[1]
         bars = load_data(filepath)
-        big_bar = get_biggest_bar(bars) - 1
-        small_bar = get_smallest_bar(bars) - 1
-        closest_bar = get_closest_bar(bars)
-        print('Самый большой бар: ', bars[big_bar]['Cells']['Name'] +
-              ' имеет ' + str(bars[big_bar]['Cells']['SeatsCount']) +
-              ' мест')
-        print('Самый маленький бар: ', bars[small_bar]['Cells']['Name'] +
-              ' имеет ' + str(bars[small_bar]['Cells']['SeatsCount']) +
-              ' мест')
-        print("Самый близкий к вам: " + bars[closest_bar]['Cells']["Name"] +
-              " имеет " + str(bars[closest_bar]['Cells']["SeatsCount"]) +
-              " мест\n"
-              'Находится по адресу: ', bars[closest_bar]['Cells']['Address'])
-
-
-
-
+        my_longitude = float(input('введите значение долготы:'))
+        my_latitude = float(input('введите значение широты:'))
+    except FileNotFoundError:
+        exit('File not found')
+    except IndexError:
+        print('Index is out of range')
+    except ValueError:
+        print('Invalid argument value')
+    print(
+        'Самый большой бар: {} имеет {} мест\n'
+        'Самый маленький бар: {} имеет {} мест\n'
+        'Самый близкий к Вам: {} имеет {} мест\n'
+        'Находится по адресу: {}'.format(
+            bars[get_biggest_bar(bars)]['Cells']['Name'],
+            bars[get_biggest_bar(bars)]['Cells']['SeatsCount'],
+            bars[get_smallest_bar(bars)]['Cells']['Name'],
+            bars[get_smallest_bar(bars)]['Cells']['SeatsCount'],
+            bars[get_closest_bar(bars)]['Cells']["Name"],
+            bars[get_closest_bar(bars)]['Cells']["SeatsCount"],
+            bars[get_closest_bar(bars)]['Cells']['Address']
+        )
+    )
 
 
 
