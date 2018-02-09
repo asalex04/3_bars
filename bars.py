2fa8e2a920f104dcbd374745864ec5eef126e450
asalex04 <arc04@mail.ru>
2018-02-09 19:04:28 +0400

    changed output function

--- bars.py
+++ bars.py
@@ -10,24 +10,23 @@ def load_data(filepath):
 
 
 def get_biggest_bar(size):
-    max_bar = max(bars, key=lambda x: x['Cells']['SeatsCount'])
-    return max_bar['Number']-1
-    # используем номер бара как индекс,
-    # для удобства вывода любой информации о баре
+    index_max_bar = max(bars, key=lambda x: x['Cells']['SeatsCount'])
+    return index_max_bar['Number']-1
 
 
 def get_smallest_bar(size):
-    min_bar = min(bars, key=lambda x: x['Cells']['SeatsCount'])
-    return min_bar['Number']-1
+    index_min_bar = min(bars, key=lambda x: x['Cells']['SeatsCount'])
+    return index_min_bar['Number']-1
 
 
-def distance(x, y, x1, y1):
+def calc_distance(x, y, x1, y1):
     dist_count = ((x - x1) ** 2 + (y - y1) ** 2) ** 0.5
     return dist_count
 
 
 def get_closest_bar(bars, longitude, latitude):
-    min_dist = min(bars, key=lambda x: distance(longitude, latitude,
+    min_dist = min(bars, key=lambda x: calc_distance(longitude,
+                                                     latitude,
                    x['Cells']['geoData']['coordinates'][0],
                    x['Cells']['geoData']['coordinates'][1]))
     return min_dist['Number']-1
@@ -39,12 +38,12 @@ if __name__ == '__main__':
         filepath = argv[1]
         bars = load_data(filepath)
     except FileNotFoundError:
-        exit('File not found')
+        exit('Файл не найден')
     try:
         my_long = float(input('введите значение долготы:'))
         my_lat = float(input('введите значение широты:'))
     except ValueError:
-        exit('Invalid argument value')
+        exit('Вы указали неверные координаты')
     print(
         'Самый большой бар: {} имеет {} мест\n'
         'Самый маленький бар: {} имеет {} мест\n'
@@ -55,8 +54,7 @@ if __name__ == '__main__':
             bars[get_smallest_bar(bars)]['Cells']['Name'],
             bars[get_smallest_bar(bars)]['Cells']['SeatsCount'],
             bars[get_closest_bar(bars, my_long, my_lat)]['Cells']["Name"],
-            bars[get_closest_bar(bars, my_long, my_lat)]['Cells']
-                                                        ["SeatsCount"],
+            bars[get_closest_bar(bars, my_long, my_lat)]['Cells']["SeatsCount"],
             bars[get_closest_bar(bars, my_long, my_lat)]['Cells']['Address']
         )
     )
